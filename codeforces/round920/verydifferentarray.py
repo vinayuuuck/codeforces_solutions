983cff5833625bb92991b3e8e1ceaf2421ca1eaa
Vinayak Singh Bhadoriya <vinayaksbhadoriya@gmail.com>
2024-01-15 16:32:28 +0000

    remove whitespace

--- codeforces/round920/verydifferentarray.py
+++ codeforces/round920/verydifferentarray.py
@@ -17,16 +17,15 @@ def maxDiff(a, b):
     # If len(a) is odd then we map the first half of a, i.e., a[0] to b[0], a[1] to b[1], etc. till a[n/2] to b[n/2]
     # and the second half of a, i.e., a[-1] to b[-1], a[-2] to b[-2], etc. till a[-n/2] to b[-n/2]
     # and then find the best possible remaining value from b to map to the middle element of a
-
+    
     if len(a) % 2 == 0:
         return sum([abs(a[i] - b[i]) for i in range(len(a) // 2)]) + sum([abs(a[-i-1] - b[-i-1]) for i in range(len(a) // 2)])
     else:
         maxdiff = 0
         for i in range(len(a) // 2):
             maxdiff += abs(a[i] - b[i]) + abs(a[-i-1] - b[-i-1])
-        
+   
         aval = a[len(a) // 2]
-        
         possiblemax = 0
         for i in range(len(a) // 2, len(b) - len(a) // 2):
             possiblemax = max(possiblemax, abs(aval - b[i]))
